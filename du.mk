a32b1c824c459aec99b1a2352d96fd111bf20dbd
daneal2u <daneal2u@gmail.com>
2016-05-25 20:02:18 +0400

    DU-ify things!
    Change-Id: I64833a16961ce1a7f23d98687629e20fab1fa275

--- du.mk
+++ du.mk
@@ -1,15 +1,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9082/device_i9082.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9082
-PRODUCT_NAME := cm_i9082
+PRODUCT_NAME := du_i9082
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9082
